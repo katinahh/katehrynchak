c6fd16deb3e675a2342548f66959a5804ff8b51d
katinahh <katinagg@gmail.com>
2019-06-08 09:13:46 +0300

    Changes

--- main.js
+++ main.js
@@ -1,23 +1,19 @@
-/*Индекс слайда по умолчанию*/
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
-/*Функция увеличивает индекс на 1, показывает следующй слайд*/
 function plusSlide() {
     showSlides(slideIndex += 1);
 }
 
-/* Функция уменьшяет индекс на 1, показывает предыдущий слайд*/
 function minusSlide() {
     showSlides(slideIndex -= 1);  
 }
 
-/* Устанавливает текущий слайд */
 function currentSlide(n) {
     showSlides(slideIndex = n);
 }
 
-/* Основная функция сладера */
 function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("item");
